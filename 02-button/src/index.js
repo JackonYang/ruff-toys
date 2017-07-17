55af126419216f0648a69f22cb575b51e41254fe
Jackon Yang <i@jackon.me>
2017-07-17 11:11:50 +0800

    button control

--- 02-button/src/index.js
+++ 02-button/src/index.js
@@ -6,9 +6,24 @@ $.ready(function (error) {
         return;
     }
 
-    $('#led-r').turnOn();
+    // Turn on `#led-r` when `#button` is pushed.
+    $('#button').on('push', function () {
+        console.log('Button pushed.');
+        $('#led-r').turnOn();
+        $('#led-g').turnOn();
+        $('#led-b').turnOff();
+    });
+
+    // Turn off `#led-r` when `#button` is released.
+    $('#button').on('release', function () {
+        console.log('Button released.');
+    $('#led-r').turnOff();
+    $('#led-g').turnOff();
+    $('#led-b').turnOn();
+    });
+
 });
 
 $.end(function () {
-    $('#led-r').turnOff();
+    $('#led-r').turnOn();
 });
